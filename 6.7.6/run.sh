4279e94bc183b061a5b504aca0c5f1a8e7e90601
Shayne Clausson <shayne.clausson@extenda.se>
2019-03-09 20:29:26 +0100

    Add support for 6.7.6

---
+++ 6.7.6/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [ "${1:0:1}" != '-' ]; then
+  exec "$@"
+fi
+
+chown -R sonarqube:sonarqube $SONARQUBE_HOME
+exec gosu sonarqube \
+  java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.search.javaAdditionalOpts="$SONARQUBE_SEARCH_JVM_OPTS" \
+  "$@"
